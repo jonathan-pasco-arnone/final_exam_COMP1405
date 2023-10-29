bb04b23a03b8cdb21d75db67deb7420a0254685b
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-10-29 13:14:19 -0400

    Update problem6.py

--- 1405-exam-materials/1405-exam-materials/problem6.py
+++ 1405-exam-materials/1405-exam-materials/problem6.py
@@ -6,8 +6,9 @@
 value_quantities = {}
 
 def process(alist):
-    """ Processes a list for later use """
+    """ Processes a dictionary from the list to be used in the within range function """
     value_quantities.clear()
+    # Adds each key and value to the dictionary
     for value in alist:
         if value_quantities.get(value) is None:
             value_quantities[value] = 1
@@ -18,6 +19,7 @@ def within_range(a, b):
     """ Finds the number of values within a given range of the previously computed list """
     total_values = 0
     counter = a
+    # Adds each dictionary value individually
     while counter <= b:
         if value_quantities.get(counter) is not None:
             total_values += value_quantities[counter]
