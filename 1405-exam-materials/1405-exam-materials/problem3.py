dfc163e797b00ba9a03fe34a1228f3141d5cdeec
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-10-29 12:57:11 -0400

    Update problem3.py

--- 1405-exam-materials/1405-exam-materials/problem3.py
+++ 1405-exam-materials/1405-exam-materials/problem3.py
@@ -4,28 +4,27 @@
 # Created on: October 2023
 
 observations = {}
-total = 0
 
 def observe(number):
     """ Observes a number one time """
+    # Adds the new number or adds 1 to the existing number
     if observations.get(number) is None:
         observations[number] = 1
     else:
         observations[number] += 1
 
-    # Accesses the global variable "total"
-    global total
-    total += 1
+    # Adds to the total number of observations
+    if observations.get("total") is None:
+        observations["total"] = 1
+    else:
+        observations["total"] += 1
 
 def probability_of(number):
     """ Determines the probability of a number in the total observations so far """
-    if observations.get(number) is None or total == 0:
+    if observations.get(number) is None or observations["total"] == 0:
         return 0
-    return observations[number] / total
+    return observations[number] / observations["total"]
 
 def reset():
     """ Resets the observations and total """
-    # Accesses the global variable "total"
-    global total
-    total = 0
     observations.clear()
