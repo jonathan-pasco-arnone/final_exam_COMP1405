8e1129fdabb7bf645ee21471d599829e666e381c
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-10-29 12:35:51 -0400

    Update problem1.py

--- 1405-exam-materials/1405-exam-materials/problem1.py
+++ 1405-exam-materials/1405-exam-materials/problem1.py
@@ -5,22 +5,25 @@
 
 FILE_PATH = "1405-exam-materials/"
 
-def median_word_length(link):
+def median_word_length(filename):
     """ Finds the median length of the words of a file """
-    file = open(FILE_PATH + link, "r", encoding="utf8")
+    file = open(FILE_PATH + filename, "r", encoding="utf8")
     contents = (file.read()).split()
 
     # Bubble Sorting
     swapped = True
     while swapped:
         swapped = False
-        for index in range(1, len(contents) - 1):
+        for index in range(1, len(contents)):
             if len(contents[index - 1]) > len(contents[index]):
                 swap_item = contents[index]
                 contents[index] = contents[index - 1]
                 contents[index - 1] = swap_item
                 swapped = True
 
+    # Calculates the median length of the middle number and the number 0.5 index's below
+    # Both indexs are rounded down to account for both even and odd quantities
     median_length = ((len(contents[int(len(contents) / 2)])
           + len(contents[int(len(contents) / 2 - 0.5)])) / 2)
+
     return median_length
