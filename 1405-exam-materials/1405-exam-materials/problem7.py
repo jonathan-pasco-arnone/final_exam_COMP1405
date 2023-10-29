46ca9bb5b85ca45e8a76578daf4f6a14090ec57a
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-10-29 13:34:55 -0400

    Update problem7.py

--- 1405-exam-materials/1405-exam-materials/problem7.py
+++ 1405-exam-materials/1405-exam-materials/problem7.py
@@ -10,9 +10,10 @@ def coldest_interval(temps, interval_length):
     for index, temperature in enumerate(temps):
         if index >= interval_length:
             # Replaces the first value of the average with the latest value
-            # This is done by first multiplying by the interval length
+            # This is done by first multiplying the average by the interval length
             # This changes it back to just the sums
             # Then we subtract the first value from the sum and add the new one
+            # Finaly we divide by the interval length again to get the new average
             average = (average * interval_length - temps[index - interval_length]
                   + temps[index]) / interval_length
             if average < coldest_average:
@@ -23,7 +24,9 @@ def coldest_interval(temps, interval_length):
             average += temperature
 
         # Calculate the first average
+        # Which would be every number from the 0th index to the interval - 1 index
         if index == interval_length - 1:
             average = average / interval_length
             coldest_average = average
+
     return coldest_average
